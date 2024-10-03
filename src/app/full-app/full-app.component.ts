9a63ea6068aaac8a382c3600362c872eed35d6df
joveem <umjoveem@gmail.com>
2024-10-03 20:24:34 -0300

    [ FAKE-STASH ]

--- src/app/full-app/full-app.component.ts
+++ src/app/full-app/full-app.component.ts
@@ -1,12 +1,21 @@
+// base libs
 import { Component } from '@angular/core';
 
+// third
+// ...
+
+// from project
+import { environment } from '../../environments/environment';
+import { EnviromentData } from '../../environments/environment';
+
 @Component({
-  selector: 'full-app',
-  standalone: true,
-  imports: [],
-  templateUrl: './full-app.component.html',
-  styleUrl: './full-app.component.css'
+    selector: 'full-app',
+    standalone: true,
+    imports: [],
+    templateUrl: './full-app.component.html',
+    styleUrl: './full-app.component.css'
 })
 export class FullAppComponent {
-
+    // dependencies
+    _environmentData: EnviromentData = environment;
 }
