59c7de5b3ade3f7afa15f34050c4041aac4e45f2
joveem <umjoveem@gmail.com>
2024-12-20 00:50:06 -0300

    adding provideHttpClient and commenting provideServiceWorker in "app.config.ts"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,18 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
-    enabled: !isDevMode(),
-    registrationStrategy: 'registerWhenStable:30000'
-  }), provideAnimationsAsync()]
+    providers: [
+        provideHttpClient(),
+        provideRouter(routes),
+        provideClientHydration(),
+        // * disabling PWA
+        // provideServiceWorker('ngsw-worker.js', {
+        //     enabled: !isDevMode(),
+        //     registrationStrategy: 'registerWhenStable:30000'
+        // }),
+        provideAnimationsAsync(),
+    ],
 };
